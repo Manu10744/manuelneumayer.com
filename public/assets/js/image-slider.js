0226603cb1be86340d4fdacc0b3c47b144b443f0
Manuel Neumayer <manuel.neumayer@campus.lmu.de>
2021-10-15 16:44:21 +0200

    update image slider logic

--- public/assets/js/image-slider.js
+++ public/assets/js/image-slider.js
@@ -1,20 +1,34 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     var splide = new Splide( '.splide', {
-      type   : 'slide',
+      type: 'slide',
       arrows: false,
+      rewind: true,
       autoplay: true,
-      autoheight: true,
-      autoheight: true,
-      width: 1080,
-      perPage: 3,
-      gap: "20px",
+      perPage: 2,
       mediaQuery: "min",
+      lazyLoad: true,
+      classes: {
+        prev  : 'splide__arrow--prev prev-slide-btn',
+		    next  : 'splide__arrow--next next-slide-btn',
+      },
       breakpoints: {
+        480: {
+          width: 400,
+          perPage: 3
+        },
         768: {
+          width: 600,
           perPage: 4
         },
         1080: {
-          perPage: 5
+          width: 700,
+          perPage: 5,
+          arrows: true,
+        },
+        1200: {
+          width: 900,
+          perPage: 5,
+          arrows: true,
         }
       }
     });
